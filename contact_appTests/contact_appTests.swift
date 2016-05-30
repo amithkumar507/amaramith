16a30f45bbc009f43f9bf9ac8934dacda0512a23
AJ <AJ@ajs-mac.local>
2016-05-29 21:43:49 -0400

    Initial Commit

---
+++ contact_appTests/contact_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  contact_appTests.swift
+//  contact_appTests
+//
+//  Created by AJ on 5/29/16.
+//  Copyright © 2016 AJ. All rights reserved.
+//
+
+import XCTest
+@testable import contact_app
+
+class contact_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
